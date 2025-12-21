7835a8e3de1c4f9588f701fecb646f0cb510c0f3
Anupriya Soni <anupriyasoni204@gmail.com>
2025-12-21 18:23:31 +0530

    Initial project commit

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const cors=require("cors");
+
+const app=express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("app is working");
+});
+
+module.exports=app;
